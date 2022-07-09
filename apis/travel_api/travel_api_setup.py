d80688fb80af9d859ac570b57a7c9dba0ad530fe
mateusb12 <mateus_b09@hotmail.com>
2022-07-09 14:15:50 -0300

    port fix 7

--- apis/travel_api/travel_api_setup.py
+++ apis/travel_api/travel_api_setup.py
@@ -61,6 +61,5 @@ def run_all():
     return run_all_queries()
 
 
+# port = int(os.environ.get('PORT', 8080))
 # app.run(host='0.0.0.0', port=8080, debug=True)
-port = int(os.environ.get('PORT', 8080))
-app.run(port=port, debug=True)
