837c492e837f5b1b82f62fb1361ed4857055dbc1
mateusb12 <mateus_b09@hotmail.com>
2022-07-15 12:16:57 -0300

    data analysis

--- pandas_manipulation/dataframe_getter.py
+++ pandas_manipulation/dataframe_getter.py
@@ -1,8 +1,16 @@
+import pandas as pd
+
 from apis.api_cruds.postgres_crud import postgres_get_all_flights_df
 
 
+def __get_flights() -> pd.DataFrame:
+    flights = postgres_get_all_flights_df()
+    flights.sort_values(by=["queryDate", "price"], inplace=True, ascending=True)
+    return flights
+
+
 def __main():
-    all_flights = postgres_get_all_flights_df()
+    all_flights = __get_flights()
     print("nice")
 
 
