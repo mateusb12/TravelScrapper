97fc3761928121a4dca6f861c1d4164072093321
mateusb12 <mateus_b09@hotmail.com>
2022-08-23 14:51:39 -0300

    Graphs improve

--- pandas_manipulation/dataframe_getter.py
+++ pandas_manipulation/dataframe_getter.py
@@ -1,16 +1,46 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import datetime
 
 from apis.api_cruds.postgres_crud import postgres_get_all_flights_df
 
 
-def __get_flights() -> pd.DataFrame:
+def get_flights() -> pd.DataFrame:
     flights = postgres_get_all_flights_df()
-    flights.sort_values(by=["queryDate", "price"], inplace=True, ascending=True)
+    flights = flights.iloc[1:]
+    flights["formatDate"] = pd.to_datetime(flights["queryDate"], format="%d-%m-%Y")
+    flights.sort_values(by=["formatDate", "price"], inplace=True, ascending=[False, True])
     return flights
 
 
+def get_recent_flights(destination: str):
+    flights = get_flights()
+    flights = flights[flights["cityTo"] == destination]
+    flights = flights.groupby(["queryDate", "price"]).first().reset_index()
+    flights.sort_values(by=["queryDate"], inplace=True, ascending=[False])
+    return flights
+
+
+def line_format(label: datetime):
+    """
+    Convert time label to the format of pandas line plot
+    """
+    day = label.day
+    month = label.month_name()[:3]
+    return f"{day}-{month}"
+
+
+def plot_graph(input_df: pd.DataFrame):
+    plt.figure()
+    plt.rcParams.update({'font.size': 16})
+    ax = input_df.plot(x="formatDate", y="price", kind="line", figsize=(15, 8), fontsize=16)
+    ax.set_xticklabels(map(line_format, input_df["formatDate"]))
+    plt.show()
+
+
 def __main():
-    all_flights = __get_flights()
+    aux = get_recent_flights("Rio de Janeiro")
+    plot_graph(aux)
     print("nice")
 
 
