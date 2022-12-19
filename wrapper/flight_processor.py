f3c846a1d808226319e90700f6f8ddff69d89faa
mateusb12 <mateus_b09@hotmail.com>
2022-12-18 21:41:32 -0300

    flight utils

---
+++ wrapper/flight_processor.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple, Dict
+
+from apis.api_consumer.kiwi_api_call import kiwi_call_example
+from datetime import datetime, timedelta
+
+from wrapper.flight_utils import analyze_layover_durations, beautify_date
+
+
+class FlightProcessor:
+    """This class is responsible for filtering and processing
+    the most important pieces of data from a flight API call"""
+
+    def __init__(self, flight_api_call: dict):
+        self.raw_data = flight_api_call
+        self.flight_amount: int = 5
+        self.data = self.raw_data.get("data")
+        self.flights = []
+        self.__flight_data_processing()
+
+    def __flight_data_processing(self):
+        variables = {"flyFrom": "departureAirport", "flyTo": "arrivalAirport", "local_departure": "departureTime",
+                     "utc_departure": "departureTimeUtc", "local_arrival": "arrivalTime",
+                     "utc_arrival": "arrivalTimeUtc", "airlines": "airlines", "duration": "duration",
+                     "baglimit": "luggageLimits", "bags_price": "luggagePrice", "price": "price",
+                     "availability": "remainingSeats", "quality": "quality", "deep_link": "link",
+                     "route": "_route"}
+        for flight in self.data:
+            flight_dict = {variables[key]: flight[key] for key in variables}
+            seats = flight["availability"]["seats"]
+            flight_dict["remainingSeats"] = 0 if seats is None else seats
+            flight_dict["numberOfStops"] = len(flight["route"])
+            flight_times = []
+            for item in flight["route"]:
+                flight_tuple = {"type": "departure", "time": item["local_departure"]}, \
+                    {"type": "arrival", "time": item["local_arrival"]}
+                flight_times.append(flight_tuple)
+            flight_durations = analyze_layover_durations(flight_times)
+            flight_dict["layoverDurations"] = flight_durations
+            flight_dict["departureTime"] = beautify_date(flight_dict["departureTime"])
+            self.flights.append(flight_dict)
+
+
+def __main():
+    flight_data = kiwi_call_example()
+    fp = FlightProcessor(flight_data)
+    return
+
+
+if __name__ == "__main__":
+    __main()
---
+++ wrapper/flight_utils.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+from typing import List, Tuple, Dict
+
+
+def analyze_layover_durations(flights: List[Tuple[Dict[str, str], Dict[str, str]]]) -> List[timedelta]:
+    waiting_times = []
+    time_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+    for index, flight in enumerate(flights):
+        if index == len(flights) - 1:
+            break
+        current_arrival = datetime.strptime(flight[1]["time"], time_format)
+        next_departure = datetime.strptime(flights[index + 1][0]["time"], time_format)
+        # Check if the current flight and the next flight are on the same day
+        if current_arrival.date() != next_departure.date():
+            # If the flights are not on the same day, adjust the current_arrival time to be the same day
+            # as the next_departure time
+            current_arrival = current_arrival.replace(year=next_departure.year, month=next_departure.month,
+                                                      day=next_departure.day)
+        waiting_time = next_departure - current_arrival
+        # Check if the waiting time is negative, indicating that the layover spans multiple days
+        if waiting_time.total_seconds() < 0:
+            # If the waiting time is negative, add a day to the current_arrival time and re-calculate the waiting time
+            current_arrival += timedelta(days=1)
+            waiting_time = next_departure - current_arrival
+        waiting_times.append(waiting_time)
+    return waiting_times
+
+
+def beautify_date(date_string: str):
+    # Parse the date string and create a datetime object
+    dt = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+    day, month, year, hour, minute = dt.day, dt.month, dt.year, dt.hour, dt.minute
+    month_names = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August',
+                   9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+    if 4 <= day <= 20 or 24 <= day <= 30:
+        suffix = 'th'
+    else:
+        suffix = ['st', 'nd', 'rd'][day % 10 - 1]
+
+    # Convert the hour to the 12-hour format
+    if hour == 0:
+        hour = 12
+        am_pm = 'AM'
+    elif 1 <= hour < 12:
+        am_pm = 'AM'
+    elif 12 <= hour < 13:
+        am_pm = 'PM'
+    else:
+        hour -= 12
+        am_pm = 'PM'
+
+    return f"{day}{suffix} {month_names[month]} {year} at {hour:02d}:{minute:02d} {am_pm}"
