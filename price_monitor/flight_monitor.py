22ee1270875b5d1da54b6bed39e796ccbf76cc11
mateusb12 <mateus_b09@hotmail.com>
2023-01-01 17:06:34 -0300

    flight monitor tests finished

--- price_monitor/flight_monitor.py
+++ price_monitor/flight_monitor.py
@@ -9,21 +9,21 @@ from price_monitor.flight_utils import get_formatted_today_date
 class FlightMonitor:
     def __init__(self):
         self.crud = FirebaseFlightCrud()
-        today_date = get_formatted_today_date()
-        self.crud.set_folder(f"flight_data/{today_date}")
+        self.today_date = get_formatted_today_date()
+        self.crud.set_folder(f"flight_data/{self.today_date}")
         self.existing_flight_data = [{}]
         self.new_flight_data = [{}]
-        self.__run()
+        # self.__run()
 
-    def __run(self):
-        self.__gather_current_data()
-        self.__collect_new_data()
-        self.__analyze_new_data()
+    def run(self):
+        self._gather_current_data()
+        self._collect_new_data()
+        self._analyze_new_data()
 
-    def __clean_folder(self):
+    def _clean_folder(self):
         self.crud.delete_folder("flight_data")
 
-    def __gather_current_data(self):
+    def _gather_current_data(self):
         raw_data = self.crud.firebase_app.get_all_flights()
         if raw_data is None:
             self.existing_flight_data = []
@@ -31,20 +31,20 @@ class FlightMonitor:
         self.existing_flight_data = list(raw_data.values())
         return
 
-    def __collect_new_data(self):
+    def _collect_new_data(self):
         kiwi_api_call = kiwi_call(fly_from="FOR", fly_to="RIO", date_from="01/01/2023",
                                   date_to="01/03/2023", limit=500)
-        trimmed_data = self.__trim_kiwi_data(kiwi_api_call["data"])
+        trimmed_data = self._trim_kiwi_data(kiwi_api_call["data"])
         flight_processor_instance = FlightProcessor(trimmed_data)
         raw_flights = flight_processor_instance.flights
         self.new_flight_data = sorted(raw_flights, key=lambda x: x["duration"]["total"])
 
     @staticmethod
-    def __trim_kiwi_data(kiwi_flights: list[dict]) -> list[dict]:
+    def _trim_kiwi_data(kiwi_flights: list[dict]) -> list[dict]:
         lowest_price_value = min(flight["price"] for flight in kiwi_flights)
         return [flight for flight in kiwi_flights if flight["price"] == lowest_price_value]
 
-    def __get_current_lowest_price(self):
+    def _get_current_lowest_price(self):
         all_flights_call = self.crud.read_all_flights()
         if all_flights_call is None:
             return float("inf")
@@ -53,22 +53,26 @@ class FlightMonitor:
         all_flights_ordered_list = sorted(all_flights_list, key=lambda flight: flight["price"])
         return all_flights_ordered_list[0]["price"]
 
-    def __analyze_new_data(self):
-        lowest_firebase_price = self.__get_current_lowest_price()
+    def _analyze_new_data(self) -> dict:
+        lowest_firebase_price = self._get_current_lowest_price()
         lowest_kiwi_price = self.new_flight_data[0]["price"]
         if lowest_firebase_price == float("inf"):
             self.crud.create_flight(self.new_flight_data[0])
-            return
+            return {"output": "failure", "outputDetails": "There was no data in the database."
+                                                          " Could not find a cheaper flight"}
         price_diff = round(100 * (lowest_kiwi_price - lowest_firebase_price) / lowest_firebase_price, 2)
         if price_diff <= -10:
             flight_link = self.new_flight_data[0]["link"]
             price_diff_tag = f"{price_diff}% cheaper"
             full_message = f"New flight found! {price_diff_tag} \n {flight_link}"
             telegram_bot_instance.send_message(chat_id=405202204, text=full_message)
-        self.__insert_new_data(self.new_flight_data)
+            return {"output": "success", "outputDetails": f"New cheapest flight found for ${lowest_kiwi_price} "
+                                                          f"({price_diff_tag} cheaper)"}
+        self._insert_new_data(self.new_flight_data)
         self.crud.firebase_app.reorder_flight_node_by_query_date_order()
+        return {"output": "failure", "outputDetails": f"Could not find a cheaper flight than {lowest_firebase_price}"}
 
-    def __insert_new_data(self, flight_pot: list[dict]):
+    def _insert_new_data(self, flight_pot: list[dict]):
         # available_flights = self.crud.trim_non_existing_flights(flight_pot)
         for flight in flight_pot:
             self.crud.create_flight(flight)
---
+++ price_monitor/tests/test_monitor.py
@@ -0,0 +1,70 @@
+import pytest
+
+from apis.api_consumer.kiwi_api_call import kiwi_call
+from price_monitor.flight_monitor import FlightMonitor
+
+
+@pytest.fixture(scope="module")
+def get_flight_monitor_instance():
+    fm = FlightMonitor()
+    today = fm.today_date
+    fm.crud.set_folder(f"test/{today}")
+    return fm
+
+
+@pytest.mark.run(order=1)
+def test_gather_current_data_handles_empty_firebase_response(get_flight_monitor_instance):
+    fm = get_flight_monitor_instance
+    fm.crud.delete_folder("test")
+    fm.run()
+    current_firebase_structure = fm.crud.firebase_app.db.reference("/").get()
+    today_flights = current_firebase_structure["test"][fm.today_date]
+    assert fm.existing_flight_data == []
+    assert len(today_flights) == 1
+
+
+@pytest.mark.run(order=2)
+def test_run_creates_new_node_when_no_stored_flights(get_flight_monitor_instance):
+    fm = get_flight_monitor_instance
+    fm.run()
+    current_firebase_structure = fm.crud.firebase_app.db.reference("/").get()
+    assert len(current_firebase_structure["test"].keys()) == 1
+    assert len(fm.existing_flight_data) == 1
+    assert len(fm.new_flight_data) >= 1
+
+
+@pytest.mark.run(order=3)
+def test_trim_kiwi_data_removes_expensive_flights(get_flight_monitor_instance):
+    fm = get_flight_monitor_instance
+    kiwi_api_call = kiwi_call(fly_from="FOR", fly_to="RIO", date_from="01/01/2023",
+                              date_to="01/03/2023", limit=500)["data"]
+    trimmed_data = fm._trim_kiwi_data(kiwi_api_call)
+    assert len(trimmed_data) < len(kiwi_api_call)
+
+
+@pytest.mark.run(order=4)
+def test_analyze_new_data_notifies_when_cheaper_flight_found(get_flight_monitor_instance):
+    fm = get_flight_monitor_instance
+    fm._gather_current_data()
+    fm._collect_new_data()
+    fm.new_flight_data[0]["price"] = 5
+    result = fm._analyze_new_data()
+    assert result["output"] == "success"
+
+
+@pytest.mark.run(order=5)
+def test_analyze_new_data_not_notify_when_no_cheaper_flight_found(get_flight_monitor_instance):
+    fm = get_flight_monitor_instance
+    fm._gather_current_data()
+    fm._collect_new_data()
+    fm.new_flight_data[0]["price"] = 100000
+    result = fm._analyze_new_data()
+    assert result["output"] == "failure"
+
+
+def __main():
+    pytest.main()
+
+
+if __name__ == "__main__":
+    __main()
